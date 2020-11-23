533a32682ee64441464f2e0af76e9766b25572da
lei.xia <2585617117@qq.com>
2020-11-23 13:05:37 +0800

    feature: 前端代码 错误上报

---
+++ code/watchError/tryCatch.js
@@ -0,0 +1,6 @@
+try {
+    var error = ‘error’
+} catch (error) {
+    console.log(error);
+    console.log('我感知不到错误');
+}
---
+++ code/watchError/window.onerror.js
@@ -0,0 +1,10 @@
+/**
+ * @param [String] errorMessage 错误信息
+ * @param [String] scriptURI 出错文件
+ * @param [Long] lineNumber 出错代码的行号
+ * @param [Long] columnNumber 出错代码的列号
+ * @param {Object} errorObj 错误的详细信息 Anything
+*/
+window.onerror = function (errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
+    
+};
